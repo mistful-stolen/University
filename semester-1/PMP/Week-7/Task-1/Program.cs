415319e17fae5516aeb084b621f22ba0a1767e4d
mistful <99610491+mistful-stolen@users.noreply.github.com>
2025-10-21 18:02:03 +0200

    Week 7. Plus notes.

---
+++ semester-1/PMP/Week-7/Task-1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("You have nothing here yet.");
+    }
+
+    static class Messages {
+        public static void Hello(){
+            Console.WriteLine("Hello.");
+        }
+
+        public static string HelloName(string name) {
+            return $"Hello, {name}.";
+        }
+    }
+
+    static class Operations {
+        public static int SumInt(params int[] a){
+            int sum = 0;
+            foreach (int element in a) {
+                sum += element;
+            }
+
+            return sum;
+        }
+    }
+}
