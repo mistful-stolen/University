67902eb4478f076b2e1392097943e77fe5010511
mistful <99610491+mistful-stolen@users.noreply.github.com>
2025-10-28 17:52:20 +0100

    Low Battery

---
+++ semester-1/PMP/Week-8/Task-1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Program {
+    class MyProgramXDD 
+    {
+        static void Main(string[] args) 
+        {
+            Car ferrari = new Car();
+            Person joe = new Person();
+            Snail pixie = new Snail();
+
+            Console.WriteLine(pixie.speed);
+
+
+            
+        }
+
+        class Person {
+            public string skinColor = "black";
+        }
+    }
+
+    class Snail {
+        public string speed = "slow";
+    }
+}
