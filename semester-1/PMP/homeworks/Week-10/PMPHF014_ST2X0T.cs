88de7ffe4f9a3d035f7cd284bb8816bf59cb34d2
mistful <99610491+mistful-stolen@users.noreply.github.com>
2025-11-11 12:06:03 +0100

    Link

--- semester-1/PMP/homeworks/Week-10/PMPHF014_ST2X0T.cs
+++ semester-1/PMP/homeworks/Week-10/PMPHF014_ST2X0T.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        // https://main.elearning.uni-obuda.hu/pluginfile.php/633579/mod_assign/introattachment/0/PMPHF014.pdf?forcedownload=1
+        
         Console.WriteLine("Hello, World!");
     }
 }
